4782cc1dce0679523d5fe9d80c10de67c13f83a5
anggawww05 <anggaputraw25@gmail.com>
2024-12-05 17:52:00 +0800

    make routing for user

--- resources/views/admin/detailAdmin.blade.php
+++ resources/views/admin/detailAdmin.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
     <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 min-h-screen">
+        <div class="p-4 border-2 border-gray-200rounded-lg min-h-screen">
             <h1>detail admin</h1>
     </div>
 @endsection
